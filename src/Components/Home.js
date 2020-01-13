deaaaf22b10a0dae3ad75b3c5b8b089c28c4f644
Mark Holmes <holmesmarkr@johndeere.com>
2020-01-13 08:48:50 -0600

    New Commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -15,8 +15,8 @@ return (
         <h1>My Name is Mark and I am a Software Engineer.</h1>
         </div>
         <div className="standard-aside">
-            <p>I have over 15 years in Information Technolongy</p>
-            <p>I have spent the majority of my career in the corprate envinroment, but have also done freelance work as well.</p>
+            <p>I have over 15 years in Information Technolongy</p><br></br>
+            <p>I am a Business Analyst turned Software Engineer because I am enjoying going to the next level. I love working with all aspects of data, finding data problems. I love seeing the flow of data from entry to the storage, to UX. I find giving data to users, for a better experience, challenging and exciting. I am able to use, new to me and industry-standard languages, to go forth and create better solutions. I recognize the value for quality in all aspects of my career and personal life and put that as my priority in all I do. I am a team player and like, and have worked in various groups in my career. </p>
         </div>
     </div>
   )
